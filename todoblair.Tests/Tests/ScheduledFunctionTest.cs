23f677b69c4a6016c2d469e9c83e3ecc52a4aa04
sebastianblaira <53913977+sebastianblaira@users.noreply.github.com>
2021-08-22 17:41:13 -0500

    Probe test

---
+++ todoblair.Tests/Tests/ScheduledFunctionTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using todoblair.Functions.Functions;
+using todoblair.Tests.Helpers;
+using Xunit;
+
+namespace todoblair.Tests.Tests
+{
+    public class ScheduledFunctionTest
+    {
+        [Fact]
+        public void ScheduledFunctionTest_Should_Log_Message()
+        {
+            // Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+
+            // Act
+            ScheduledFunction.Run(null, mockTodos, logger);
+            string message = logger.Logs[0];
+
+            // Assert
+            Assert.Contains("Deleting completed", message);
+        }
+    }
+}
